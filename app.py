fa03a500c07b7d5c51ca6d4ddbb66bcc9c3d6b09
Alan Meigs <drakosfire@gmail.com>
2024-08-12 12:23:20 -0600

    updated app.py flask app launch to point to host 0.0.0.0 and port 7860 to match huggingface, hoping this resolves CORS issue

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def generate_image():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
