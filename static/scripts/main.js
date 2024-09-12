81fb73952992de41a269c416c54931e5bd3f464d
Alan Meigs <drakosfire@gmail.com>
2024-09-12 16:24:37 -0600

    Missed adding main.js to the last commit

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,35 +1,9 @@
 // main.js
 
 import { initializeDOMElements } from '/static/scripts/domInit.js';
-import { getState, updateState } from './state.js';
-
+import { getState } from './state.js';
 import { setupEventListeners } from '/static/scripts/eventHandlers.js';
-import {changeImage, startLoadingAnimation, stopLoadingAnimation } from '/static/scripts/loadingImage.js';
-import {toggleAllTextBlocks,
-        autofillBlocks,
-        storeInitialPositions,
-        sortBlocksById,
-        reinsertBlock,
-        insertHtmlBlocks,
-        extractBlocks
-        } from '/static/scripts/blockHandler.js';
-import {handleDragStart, handleDragEnd, handleDragOver, handleDrop} from '/static/scripts/dragDropHandler.js';
-import {getColumnFromOffset,
-        getColumnHeights,
-        adjustPageLayout,
-        addPage,
-        removePage,
-        handleColumnOverflow,
-        getNextPage
-        } from '/static/scripts/pageHandler.js';
-import {handleTrashDrop, handleTrashOver, handleTrashLeave} from '/static/scripts/trashHandler.js';
-import {adjustTextareaHeight,
-        initializeTextareaResizing,
-        lockTextareas,
-        unlockTextareas
-        } from '/static/scripts/handleTextareas.js';
-import { handleClick,generateImage } from '/static/scripts/eventHandlers.js';
-import { handleReset, printScreen } from '/static/scripts/utils.js';
+import { extractBlocks } from '/static/scripts/blockHandler.js';
 import { initialLoadJSON, loadHandler } from '/static/scripts/loadJSONHandler.js';
 
 document.addEventListener('DOMContentLoaded', async function() {
