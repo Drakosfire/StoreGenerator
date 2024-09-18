6718d54b2624a9bcc0ad60e0b1b8a5b559ec5503
Drakosfire <drakosfire@gmail.com>
2024-09-17 20:55:17 -0600

    Updated prompt to simplify

--- sd_generator.py
+++ sd_generator.py
@@ -1,9 +1,9 @@
 import fal_client
-def preview_and_generate_image(image_subject, image_subject_name ,sd_prompt):   
+def preview_and_generate_image(sd_prompt):   
     handler = fal_client.submit(
         "fal-ai/flux/dev",
         arguments={
-            "prompt": f"A fantasy image of a {image_subject} in a fantasy world, the name of the store is {image_subject_name} it looks likes {sd_prompt}",
+            "prompt": f"{sd_prompt}",
             "num_inference_steps": 28,
             "guidance_scale": 3.5,
 
