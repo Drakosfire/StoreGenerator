2732873c6711e1406bac902fb4afdf473861c069
Alan Meigs <drakosfire@gmail.com>
2024-09-19 22:05:56 -0600

    moved elements to a functions in utils.js

--- static/scripts/domInit.js
+++ static/scripts/domInit.js
@@ -1,22 +1,9 @@
 // domInit.js
 import { updateState, getState } from './state.js';
+import { buildElements } from './utils.js';
 // Function to initialize and retrieve necessary DOM elements
 export function initializeDOMElements() {
-    const elements = {};
-
-    // Retrieve and assign DOM elements
-    elements.blockContainer = document.getElementById('blockContainer');
-    elements.blockContainerPage = document.getElementById('block-page');
-    elements.pageContainer = document.getElementById('pages');
-    elements.trashArea = document.getElementById('trashArea');
-    elements.currentPage = elements.pageContainer ? elements.pageContainer.querySelector('.block.monster.frame.wide') : null;
-    elements.modal = document.getElementById('imageModal');
-    elements.modalImg = document.getElementById('modalImage');
-    elements.captionText = document.getElementById('caption');
-    elements.closeModal = document.getElementsByClassName('close')[0];
-    elements.loadingImage = document.getElementById('loadingImage');
-
-    // Note: Dynamic elements (e.g., sdPromptElement, imageElement) might need specific handling depending on when they are created.
+    const elements = buildElements();
 
     // Check for null elements and log errors
     for (const [key, value] of Object.entries(elements)) {
